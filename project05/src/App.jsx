98e082407b73e59b1636ae90e5ea342d0c99937e
Ayush <namea4242@gmail.com>
2024-12-20 00:45:44 +0530

    Add project05 (Theme toggling)

---
+++ project05/src/App.jsx
@@ -0,0 +1,40 @@
+
+import { useEffect, useState } from 'react';
+import Card from './components/Card';
+import ThemeBtn from './components/Themebtn';
+import { ThemeProvider } from './context/Themecontext';
+
+function App() {
+  const [themeMode, setThemeMode] = useState('light')
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+  
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  useEffect(() => {
+    const html = document.querySelector('html')
+    html.classList.remove('dark','light')
+    html.classList.add(themeMode)
+  },[themeMode])
+
+  return (
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full  max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App
